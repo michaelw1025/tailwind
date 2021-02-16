d56b38491803baae9b6640d441f85c92ed9df277
Mike <michaelw1025@gmail.com>
2021-02-16 14:42:50 -0600

    colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,24 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
-    colors: {},
+    colors: {
+      gray: colors.gray,
+      red: colors.red,
+      orange: colors.orange,
+      yellow: colors.yellow,
+      green: colors.green,
+      teal: colors.teal,
+      lightBlue: colors.lightBlue,
+      blue: colors.blue,
+      purple: colors.purple,
+      white: {
+        DEFAULT: 'ffffff',
+      }
+    },
   },
   variants: {
     extend: {},
